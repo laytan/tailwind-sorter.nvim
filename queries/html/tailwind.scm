30f3e66cc2360ce5b2dc3ea3ac26ea34f50c03e3
Laytan Laats <laytanlaats@hotmail.com>
2023-02-11 23:05:10 +0100

    feat: add support for AlpineJS x-transition classes

--- queries/html/tailwind.scm
+++ queries/html/tailwind.scm
@@ -1,5 +1,12 @@
+; Regular classes
 (attribute
   (attribute_name) @_name
     (#eq? @_name "class")
+  (quoted_attribute_value
+    (attribute_value) @tailwind))
+
+; AlpineJS
+(attribute
+  (attribute_name) @_name (#match? @_name "^x-transition:[a-z-]*$")
   (quoted_attribute_value
     (attribute_value) @tailwind))

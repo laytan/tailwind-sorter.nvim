f3e07511788b787110c62d4dde463a7fb736ddde
Laytan Laats <laytanlaats@hotmail.com>
2023-09-12 19:15:36 +0200

    feat: php string content sorting

---
+++ tests/fixtures/automatic/test.php
@@ -0,0 +1,6 @@
+<div class="grid text-slate-100 test">
+  <p class="<?= 'grid testing flex' ?>">
+<!-- PHP embeds HTML and strings
+<div class="test grid text-slate-100">
+  <p class="<?= 'testing flex grid' ?>">
+-->

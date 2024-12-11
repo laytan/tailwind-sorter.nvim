bb88ff612f9151eeb12fbf6e1a5c38df5a95f4c6
phenpessoa <phenpessoa@gmail.com>
2024-12-11 17:32:06 -0300

    update templ query to match on more possibilities of the class attribute

--- queries/templ/tailwind.scm
+++ queries/templ/tailwind.scm
@@ -1,1 +1,60 @@
 ; inherits: html
+
+(attribute
+  (attribute_name) @_name
+    (#eq? @_name "class")
+  [
+    (dynamic_class_attribute_value
+      [
+        (interpreted_string_literal
+          (interpreted_string_literal_content) @tailwind)
+
+        (raw_string_literal
+          (raw_string_literal_content) @tailwind)
+
+        (binary_expression
+          [
+            [
+              (interpreted_string_literal
+                (interpreted_string_literal_content) @tailwind)
+              (raw_string_literal
+                (raw_string_literal_content) @tailwind)
+            ]
+            [
+              (interpreted_string_literal
+                (interpreted_string_literal_content) @tailwind)
+              (raw_string_literal
+                (raw_string_literal_content) @tailwind)
+            ]
+          ]
+        )
+      ]
+    )
+    (expression
+      [
+        (interpreted_string_literal
+          (interpreted_string_literal_content) @tailwind)
+
+        (raw_string_literal
+          (raw_string_literal_content) @tailwind)
+
+        (binary_expression
+          [
+            [
+              (interpreted_string_literal
+                (interpreted_string_literal_content) @tailwind)
+              (raw_string_literal
+                (raw_string_literal_content) @tailwind)
+            ]
+            [
+              (interpreted_string_literal
+                (interpreted_string_literal_content) @tailwind)
+              (raw_string_literal
+                (raw_string_literal_content) @tailwind)
+            ]
+          ]
+        )
+      ]
+    )
+  ]
+)
